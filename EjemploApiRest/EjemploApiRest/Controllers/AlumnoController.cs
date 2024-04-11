e5748eb29f3d28d9b8455ec022e92005cb33d13f
EmiBuffet <emibuffet@gmail.com>
2024-04-11 19:25:16 -0300

    Clase 202401104

---
+++ EjemploApiRest/EjemploApiRest/Controllers/AlumnoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EjemploApiRest.Controllers
+{
+    [ApiController]
+    [Route("api/alumnos")]
+    public class AlumnoController : ControllerBase
+    {
+        static List<Alumno> alumnoList = new List<Alumno>
+        {
+            new Alumno(1, "Juan", "Perez"),
+            new Alumno(2, "Lionel", "Messi")
+        };
+
+        [HttpGet]
+        public List<Alumno> ConsultarAlumnos()
+        {
+            return alumnoList;
+        }
+    }
+}
