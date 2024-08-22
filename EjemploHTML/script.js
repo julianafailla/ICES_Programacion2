d7c4823c3adcfb9d11b23983c895ebe90b8f1292
EmiBuffet <emibuffet@gmail.com>
2024-08-22 17:00:34 -0300

    Clase 202408

--- EjemploHTML/script.js
+++ EjemploHTML/script.js
@@ -1,3 +1,10 @@
 function miFuncion() {
-    alert('Funciono!');
+    let nombre = document.getElementById('nombre').value
+    let apellido = document.getElementById('apellido').value
+
+    let nombre_mas_apellido = nombre + ' ' + apellido
+
+    document.getElementById('nombreapellido').innerText = nombre_mas_apellido;
+
+    alert(nombre_mas_apellido);
 }
